f63f083c25cb4dfcec655fdcdf9e678a06190db6
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2025-06-16 21:37:24 +0300

    0. Build a Dynamic To-Do List Application

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const taskList = document.getElementById("task-list")
     const taskInput = document.getElementById("task-input")
 
-    const addTask = () => {
+    function addTask() {
         const taskText = taskInput.value.trim()
         if (taskText === "") {
             alert("enter a task")
